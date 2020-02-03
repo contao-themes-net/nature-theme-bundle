09dfd05bd60824b5c8aafed88ac5f06bcd66be74
Philipp Seibt <seibt@pdir.de>
2020-02-03 16:30:23 +0100

    update social feed templates

--- src/Module/NatureThemeSetup.php
+++ src/Module/NatureThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\NatureThemeBundle\Module;
 
 class NatureThemeSetup extends \BackendModule
 {
-    const VERSION = '1.1.3';
+    const VERSION = '1.1.4';
 
     protected $strTemplate = 'be_naturetheme_setup';
 
