6633f9a44497df9c165ea5025a544539f0158140
Philipp Seibt <seibt@pdir.de>
2020-06-26 14:09:21 +0200

    update feature element

--- src/Module/NatureThemeSetup.php
+++ src/Module/NatureThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\NatureThemeBundle\Module;
 
 class NatureThemeSetup extends \BackendModule
 {
-    const VERSION = '1.2.10';
+    const VERSION = '1.3.1';
 
     protected $strTemplate = 'be_naturetheme_setup';
 
