5a724e807d9241662e721e16e7d3224bd0684375
Philipp Seibt <seibt@pdir.de>
2020-12-04 16:39:47 +0100

    update version

--- src/Module/NatureThemeSetup.php
+++ src/Module/NatureThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\NatureThemeBundle\Module;
 
 class NatureThemeSetup extends \BackendModule
 {
-    const VERSION = '1.3.5';
+    const VERSION = '1.3.6';
 
     protected $strTemplate = 'be_naturetheme_setup';
 
