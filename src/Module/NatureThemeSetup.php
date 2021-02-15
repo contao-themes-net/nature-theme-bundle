6c970b757b7c54e668aac69313f2d306995c9390
Philipp Seibt <seibt@pdir.de>
2021-02-15 10:16:53 +0100

    css fix for fullheight teaserbox

--- src/Module/NatureThemeSetup.php
+++ src/Module/NatureThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\NatureThemeBundle\Module;
 
 class NatureThemeSetup extends \BackendModule
 {
-    const VERSION = '1.4.0';
+    const VERSION = '1.4.1';
 
     protected $strTemplate = 'be_naturetheme_setup';
 
