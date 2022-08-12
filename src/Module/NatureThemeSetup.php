bab65166282d089101ebca0a9625e90b2d166442
Mathias Arzberger <develop@pdir.de>
2022-08-12 21:22:37 +0200

    move files folder; add new sql files for contao 5

--- src/Module/NatureThemeSetup.php
+++ src/Module/NatureThemeSetup.php
@@ -19,14 +19,11 @@ declare(strict_types=1);
 namespace ContaoThemesNet\NatureThemeBundle\Module;
 
 use Contao\BackendModule;
-use Contao\File;
-use Contao\Folder;
-use Contao\Input;
 use ContaoThemesNet\NatureThemeBundle\ThemeUtils;
 
 class NatureThemeSetup extends BackendModule
 {
-    public const VERSION = '1.9.0';
+    public const VERSION = '2.0.0';
 
     protected $strTemplate = 'be_naturetheme_setup';
 
@@ -35,72 +32,6 @@ class NatureThemeSetup extends BackendModule
      */
     protected function compile(): void
     {
-        switch (Input::get('act')) {
-            case 'syncFolder':
-                $path = sprintf(
-                    '%s/%s/bundles/contaothemesnetnaturetheme',
-                    ThemeUtils::getRootDir(),
-                    ThemeUtils::getWebDir()
-                );
-
-                if (!file_exists('files/naturetheme')) {
-                    new Folder('files/naturetheme');
-                }
-                $this->getFiles($path);
-                $this->getSqlFiles(ThemeUtils::getRootDir().'/vendor/contao-themes-net/nature-theme-bundle/src/templates');
-                $this->Template->message = true;
-                $this->Template->version = self::VERSION;
-                break;
-
-            case 'truncateTlFiles':
-                $this->import('Database');
-                $this->Database->prepare('TRUNCATE tl_files')->execute();
-                $this->Template->messageTruncate = true;
-                $this->Template->version = self::VERSION;
-                break;
-
-            default:
-                $this->Template->version = self::VERSION;
-        }
-    }
-
-    protected function getFiles($path): void
-    {
-        foreach (scan($path) as $dir) {
-            if (!is_dir($path.'/'.$dir)) {
-                $pos = strpos($path, 'contaothemesnetnaturetheme');
-                $filesFolder = 'files/naturetheme'.str_replace('contaothemesnetnaturetheme', '', substr($path, $pos)).'/'.$dir;
-
-                if ('_nature_variables.scss' !== $dir && '_nature_colors.scss' !== $dir && 'backend.css' !== $dir && 'nature.scss' !== $dir && 'nature_win.scss' !== $dir && 'nature_style.scss' !== $dir && 'maklermodul.scss' !== $dir && 'fonts.scss' !== $dir) {
-                    if (!file_exists(ThemeUtils::getRootDir().'/'.$filesFolder)) {
-                        $objFile = new File(ThemeUtils::getWebDir().'/bundles/'.substr($path, $pos).'/'.$dir, true);
-                        $objFile->copyTo($filesFolder);
-                    }
-                }
-            } else {
-                $folder = $path.'/'.$dir;
-                $pos = strpos($path, 'contaothemesnetnaturetheme');
-                $filesFolder = 'files/naturetheme'.str_replace('contaothemesnetnaturetheme', '', substr($path, $pos)).'/'.$dir;
-
-                if ('bulma' !== $dir && 'parts' !== $dir && 'fonts' !== $dir && 'js' !== $dir && 'css' !== $dir && 'color_schemes' !== $dir) {
-                    if (!file_exists($filesFolder)) {
-                        new Folder($filesFolder);
-                    }
-                    $this->getFiles($folder);
-                }
-            }
-        }
-    }
-
-    protected function getSqlFiles($path): void
-    {
-        foreach (scan($path) as $dir) {
-            if (!is_dir($path.'/'.$dir)) {
-                $pos = strpos($path, '/vendor');
-                $filesFolder = 'templates/'.$dir;
-                $objFile = new File(substr($path, $pos).'/'.$dir, true);
-                $objFile->copyTo($filesFolder);
-            }
-        }
+        $this->Template->version = self::VERSION;
     }
 }
