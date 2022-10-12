59e35587b217b7c82e8852f7e0b38daa076111ea
seibt <seibt@pdir.de>
2022-10-12 15:57:32 +0200

    update header image migration

--- src/Migration/Version200.php
+++ src/Migration/Version200.php
@@ -68,12 +68,12 @@ class Version200 extends AbstractMigration
         $test = $this->connection->fetchOne("SELECT id FROM tl_image_size WHERE name = 'Headerbild &#40;Fixed&#41;'");
 
         if(false === $test) {
-            $this->connection->executeStatement("INSERT INTO `tl_image_size` (`id`, `skipIfDimensionsMatch`, `lazyLoading`, `pid`, `tstamp`, `name`, `cssClass`, `densities`, `sizes`, `width`, `height`, `resizeMode`, `zoom`, `formats`) VALUES (NULL, '0', '1', '1', '1665577831', 'Headerbild &#40;Fixed&#41;', '', '', '1x, 1.5x, 2x', '1920', '560', 'crop', '0', 'a:3:{i:0;s:12:\"png:webp,png\";i:1;s:27:\"jpg:webp,jpg;jpeg:webp,jpeg\";i:2;s:12:\"gif:webp,gif\";}');");
+            $this->connection->executeStatement("INSERT INTO `tl_image_size` (`id`, `skipIfDimensionsMatch`, `lazyLoading`, `pid`, `tstamp`, `name`, `cssClass`, `densities`, `sizes`, `width`, `height`, `resizeMode`, `zoom`, `formats`) VALUES (NULL, '0', '1', '1', '1665577831', 'Headerbild &#40;Fixed&#41;', '', '1x, 1.5x, 2x', '', '1920', '560', 'crop', '0', 'a:3:{i:0;s:12:\"png:webp,png\";i:1;s:27:\"jpg:webp,jpg;jpeg:webp,jpeg\";i:2;s:12:\"gif:webp,gif\";}');");
 
             $lastInsertId = $this->connection->lastInsertId();
             $this->connection->executeStatement("UPDATE tl_content SET size = 'a:3:{i:0;s:0:\"\";i:1;s:0:\"\";i:2;s:2:\"".$lastInsertId."\";}' WHERE (size = '' OR size = 'a:3:{i:0;s:0:\"\";i:1;s:0:\"\";i:2;s:0:\"\";}') AND customTpl = 'content_element/image/header_image_nature';");
 
-            $this->connection->executeStatement("INSERT INTO `tl_image_size_item` (`id`, `invisible`, `pid`, `sorting`, `tstamp`, `media`, `densities`, `sizes`, `width`, `height`, `resizeMode`, `zoom`) VALUES (NULL, '1', ".$lastInsertId.", '128', '1665577834', '(max-width:1023px)', '', '1x, 1.5x, 2x', '1023', '560', 'crop', NULL), (NULL, '1', ".$lastInsertId.", '64', '1665577833', '(max-width:768px)', '', '1x, 1.5x, 2x', '768', '360', 'crop', NULL), (NULL, '1', ".$lastInsertId.", '32', '1665577893', '(max-width:450px)', '', '1x, 1.5x, 2x', '450', '360', 'crop', NULL);");
+            $this->connection->executeStatement("INSERT INTO `tl_image_size_item` (`id`, `invisible`, `pid`, `sorting`, `tstamp`, `media`, `densities`, `sizes`, `width`, `height`, `resizeMode`, `zoom`) VALUES (NULL, '0', ".$lastInsertId.", '128', '1665577834', '(max-width:1023px)', '1x, 1.5x, 2x', '', '1023', '560', 'crop', NULL), (NULL, '0', ".$lastInsertId.", '64', '1665577833', '(max-width:768px)', '1x, 1.5x, 2x', '', '768', '360', 'crop', NULL), (NULL, '0', ".$lastInsertId.", '32', '1665577893', '(max-width:450px)', '1x, 1.5x, 2x', '', '450', '360', 'crop', NULL);");
         }
 
         return $this->createResult(true);
