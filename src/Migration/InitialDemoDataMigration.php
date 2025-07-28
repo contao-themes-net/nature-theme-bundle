7e2513ea063ec88b43091a43cfcd84ae625e5fe7
Philipp Seibt <seibt@pdir.de>
2025-07-28 11:39:12 +0200

    update sql import for contao 5.6.x-dev

--- src/Migration/InitialDemoDataMigration.php
+++ src/Migration/InitialDemoDataMigration.php
@@ -58,7 +58,7 @@ class InitialDemoDataMigration extends AbstractMigration
         }
 
         // Check wich Contao Version is used
-        if (version_compare(ContaoCoreBundle::getVersion(), '5.6.0', '>=')) {
+        if (version_compare(ContaoCoreBundle::getVersion(), '5.6.0', '>=') || '5.6.x-dev' === ContaoCoreBundle::getVersion()) {
             $this->sqlFile = str_replace('contao53', 'contao56', $this->sqlFile);
         }
 
